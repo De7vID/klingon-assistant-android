5dc2544265b96b5f45f40f90fea9040e319c93ff
De'vID <De.vID.jonpIn@gmail.com>
2020-12-02 15:23:42 +0000

    [MAJOR CHANGE] version 2020.12.02a - Add Finnish (fi) language.

--- app/src/main/java/org/tlhInganHol/android/klingonassistant/KlingonContentDatabase.java
+++ app/src/main/java/org/tlhInganHol/android/klingonassistant/KlingonContentDatabase.java
@@ -193,7 +193,7 @@ public class KlingonContentDatabase {
 
   // This should be kept in sync with the version number in the database
   // entry {boQwI':n} of the database which is bundled into the app.
-  private static final int BUNDLED_DATABASE_VERSION = 202009070;
+  private static final int BUNDLED_DATABASE_VERSION = 202012020;
 
   // Metadata about the installed database, and the updated database, if any.
   public static final String KEY_INSTALLED_DATABASE_VERSION = "installed_database_version";
