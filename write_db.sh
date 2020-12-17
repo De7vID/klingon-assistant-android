9d3f22bb6f2e666553738449899a8970e965a836
De'vID <De.vID.jonpIn@gmail.com>
2020-12-17 17:56:58 +0000

    Ensure no uncommitted changes in data submodule before writing database.

--- write_db.sh
+++ write_db.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 cd data
+git stash push --quiet
+git reset --hard
 ./strip_non_en_de.sh
 ./generate_db.sh --noninteractive
 cp qawHaq.db ../app/src/main/assets/
 ./stats.sh
+git reset --hard
 cd ..
